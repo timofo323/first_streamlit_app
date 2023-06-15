66bbc019a8a0d3a75a2b73705310f24a1d4ce6cc
timofo323 <132470679+timofo323@users.noreply.github.com>
2023-06-15 11:24:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Driver')
 
-streamlit.header('reakfast Favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
