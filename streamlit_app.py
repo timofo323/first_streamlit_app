2c45aeaf040554406336fc3829d24d07182cb916
timofo323 <132470679+timofo323@users.noreply.github.com>
2023-06-19 16:32:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", my_data_rows)
+streamlit.text(my_data_rows[add_my_fruit])
