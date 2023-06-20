f1a41e3b2d636db5bb1425c58f19061709522b37
timofo323 <132470679+timofo323@users.noreply.github.com>
2023-06-20 15:17:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
-add_my_fruit = streamlit.multiselect("What fruit would you like to add?", my_data_rows)
-streamlit.text(my_data_rows[add_my_fruit])
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+streamlit.text(fruit_choice)
